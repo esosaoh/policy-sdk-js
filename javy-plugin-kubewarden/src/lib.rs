ee50f8f53adf8fbb377ccf790bb583ef8f247ce6
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-09-26 22:49:25 -0400

    chore: fix linter issues

--- javy-plugin-kubewarden/src/lib.rs
+++ javy-plugin-kubewarden/src/lib.rs
@@ -14,47 +14,56 @@ fn config() -> Config {
 }
 
 fn modify_runtime(runtime: Runtime) -> Runtime {
-    runtime.context().with(|ctx| {
-        ctx.globals().set(
-            "policyAction",
-            Func::from(|| {
-                let args = std::env::args().collect::<Vec<String>>();
-                if args.len() != 2 {
-                    // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
-                    return Err(javy_plugin_api::javy::quickjs::Error::Unknown);
-                }
-                Ok(args[1].clone())
-            }),
-        )
-    })
-    .unwrap();
-    
-    runtime.context().with(|ctx| {
-        ctx.globals().set(
-            "__hostCall",
-            Func::from(|binding: String, ns: String, op: String, msg: javy_plugin_api::javy::quickjs::Object| {
-                let msg = msg
-                    .as_array_buffer()
-                    .and_then(|ab| ab.as_bytes())
-                    .ok_or(javy_plugin_api::javy::quickjs::Error::Unknown)?; // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
+    runtime
+        .context()
+        .with(|ctx| {
+            ctx.globals().set(
+                "policyAction",
+                Func::from(|| {
+                    let args = std::env::args().collect::<Vec<String>>();
+                    if args.len() != 2 {
+                        // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
+                        return Err(javy_plugin_api::javy::quickjs::Error::Unknown);
+                    }
+                    Ok(args[1].clone())
+                }),
+            )
+        })
+        .unwrap();
 
-                let successful = crate::call(
-                    binding.as_ptr() as u32,
-                    binding.len() as u32,
-                    ns.as_ptr() as u32,
-                    ns.len() as u32,
-                    op.as_ptr() as u32,
-                    op.len() as u32,
-                    msg.as_ptr() as u32,
-                    msg.len() as u32,
-                );
+    runtime
+        .context()
+        .with(|ctx| {
+            ctx.globals().set(
+                "__hostCall",
+                Func::from(
+                    |binding: String,
+                     ns: String,
+                     op: String,
+                     msg: javy_plugin_api::javy::quickjs::Object| {
+                        let msg = msg
+                            .as_array_buffer()
+                            .and_then(|ab| ab.as_bytes())
+                            .ok_or(javy_plugin_api::javy::quickjs::Error::Unknown)?; // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
 
-                Ok::<bool, javy_plugin_api::javy::quickjs::Error>(successful == 0)
-            }),
-        )
-    })
-    .unwrap();
-    
+                        let successful = crate::call(
+                            binding.as_ptr() as u32,
+                            binding.len() as u32,
+                            ns.as_ptr() as u32,
+                            ns.len() as u32,
+                            op.as_ptr() as u32,
+                            op.len() as u32,
+                            msg.as_ptr() as u32,
+                            msg.len() as u32,
+                        );
+
+                        Ok::<bool, javy_plugin_api::javy::quickjs::Error>(successful == 0)
+                    },
+                ),
+            )
+        })
+        .unwrap();
+
     runtime
 }
 
@@ -62,6 +71,11 @@ struct Component;
 
 // Dynamically linked modules will use `kubewarden-javy-plugin-v1` as the import
 // namespace.
-javy_plugin!("kubewarden-javy-plugin-v1", Component, config, modify_runtime);
+javy_plugin!(
+    "kubewarden-javy-plugin-v1",
+    Component,
+    config,
+    modify_runtime
+);
 
 export!(Component);
